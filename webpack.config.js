90ae2db78e81c2163a7130c87b2365ad514e6e51
Eliraz Atia <elirazatia003@gmail.com>
2022-05-27 17:40:21 +0100

    Fixed process is not defined error due to webpack.config.js misconfiguration

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require("path");
+const webpack = require("webpack");
+const process = require('process')
 
 module.exports = {
     entry: './index.js',
     optimization:{
       minimize:false
     },
+    plugins:[
+      new webpack.ProvidePlugin({
+        // Make a global `process` variable that points to the `process` package,
+        // because the `util` package expects there to be a global variable named `process`.
+             // Thanks to https://stackoverflow.com/a/65018686/14239942
+        process: 'process/browser'
+     })
+    ],
     output: {
       filename: 'index.js',
       path: path.resolve(__dirname, 'dist'),
