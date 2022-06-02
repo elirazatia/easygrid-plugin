fc79ee9cc66fd10bc88449b06b48269c0ada922c
Eliraz Atia <elirazatia003@gmail.com>
2022-06-02 19:16:19 +0100

    Added a failsafe window.postmessage that sends an empty selection incase of Figma event one not working

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
  * Import the UI element
  */
 import _ from "./scripts/root/root"
+
+/**
+ * Make sure that selection is empty on launch
+ */
+window.postMessage({
+    pluginMessage: {
+        type: 'selectionchange',
+        item: null
+    }
+}, '*')
